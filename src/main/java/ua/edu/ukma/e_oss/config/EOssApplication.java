45dd8325d5fe86a15a5a5dee71eba7055863068e
Oleksii <aar97@ukr.net>
2018-10-13 18:09:58 +0300

    Config mvc

--- src/main/java/ua/edu/ukma/e_oss/config/EOssApplication.java
+++ src/main/java/ua/edu/ukma/e_oss/config/EOssApplication.java
@@ -3,7 +3,7 @@ package ua.edu.ukma.e_oss.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ua.edu.ukma.*")
 public class EOssApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EOssApplication.class, args);
--- src/main/java/ua/edu/ukma/e_request/controller/MainController.java
+++ src/main/java/ua/edu/ukma/e_request/controller/MainController.java
@@ -1,11 +1,18 @@
 package ua.edu.ukma.e_request.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 /**
  * To be renamed
  */
 @Controller
+@RequestMapping("/e_request")
 public class MainController {
+    @GetMapping({"", "/"})
+    public String renderPage(){
+        return "e_request/test";
+    }
 }
