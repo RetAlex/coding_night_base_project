f803d7d0a680ac141e00e7bc080bf99fc8e2ee4d
Oleksii <aar97@ukr.net>
2018-10-13 19:43:33 +0300

    Added controllers for creating order

---
+++ src/main/java/ua/edu/ukma/e_request/controller/CreateOrderController.java
@@ -0,0 +1,33 @@
+package ua.edu.ukma.e_request.controller;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.edu.ukma.e_request.services.interfaces.RequestService;
+
+import javax.validation.Valid;
+
+@RequestMapping("/e_request")
+public class CreateOrderController {
+    private final RequestService requestService;
+
+    public CreateOrderController(RequestService requestService) {
+        this.requestService = requestService;
+    }
+
+    @GetMapping({"", "/"})
+    public String renderPage(){
+        return "e_request/creation/create_request";
+    }
+
+    @PostMapping("/create")
+    public String createRequest(@Valid @ModelAttribute CreateRequestForm createRequestForm, BindingResult bindingResult){
+        return "e_request/creation/request_created";
+    }
+
+    public static class CreateRequestForm{
+
+    }
+}
