368ee247b16fd2b6ca10c3e8f9cc81d4b6624f50
Oleksii <aar97@ukr.net>
2018-10-14 00:32:06 +0300

    Enabled JPA and entities

--- src/main/java/ua/edu/ukma/e_oss/config/EOssApplication.java
+++ src/main/java/ua/edu/ukma/e_oss/config/EOssApplication.java
@@ -2,10 +2,14 @@ package ua.edu.ukma.e_oss.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "ua.edu.ukma.*")
 @EnableConfigurationProperties
+@EnableJpaRepositories(basePackages = "ua.edu.ukma.e_request.repositories")
+@EntityScan(basePackages = "ua.edu.ukma.e_request.entities")
 public class  EOssApplication {
 
 	public static void main(String[] args) {
