4c25fb358fdc36212efd37e453ca5d0569b9801a
AstiaSun <nastiasunrise@gmail.com>
2018-10-13 20:14:07 +0300

    add user entity

---
+++ src/main/java/ua/edu/ukma/e_request/entities/Role.java
@@ -0,0 +1,8 @@
+package ua.edu.ukma.e_request.entities;
+
+/**
+ * Created by a.bondarenko on 10/13/2018.
+ */
+public enum Role {
+    STUDENT, TEACHER, MENTOR, DEKAN
+}
---
+++ src/main/java/ua/edu/ukma/e_request/entities/User.java
@@ -0,0 +1,70 @@
+package ua.edu.ukma.e_request.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Test User instance
+ * Created by a.bondarenko on 10/13/2018.
+ */
+@Entity
+public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 8)
+    private Role role;
+    private String email;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
