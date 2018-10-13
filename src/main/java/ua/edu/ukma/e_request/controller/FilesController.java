c111b070a35d03fb7297fffde53498344c8a2b8e
Yuliia Podpruhina <yuliia@MacBook-Pro-Yuliia.local>
2018-10-13 21:33:32 +0300

    testing view for file uploading

--- src/main/java/ua/edu/ukma/e_request/controller/FilesController.java
+++ src/main/java/ua/edu/ukma/e_request/controller/FilesController.java
@@ -2,15 +2,14 @@ package ua.edu.ukma.e_request.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ua.edu.ukma.e_request.services.FileStorageService;
 
 import java.io.IOException;
 
-@RestController
+@Controller
+@RequestMapping("/e_request")
 public class FilesController {
 
     private final FileStorageService fileStorageService;
@@ -20,17 +19,22 @@ public class FilesController {
         this.fileStorageService = fileStorageService;
     }
 
+    @GetMapping("/upload")
+    public String renderPage(){
+        return "e_request/view/uploadfile";
+    }
+
     @PostMapping("/uploadfile")
-    public Model saveFiles(@RequestParam("file") MultipartFile file, Model model, @RequestParam long userId, long eventId) {
+    public String saveFiles(@RequestParam("file") MultipartFile file, Model model, @RequestParam long userId, long eventId) {
         if (file.isEmpty()) {
             model.addAttribute("error", "File is empty!");
-            return model;
+            return "redirect:/e_request/uploadfile";
         }
         try {
             fileStorageService.saveFile(userId, eventId, file.getBytes(), file.getOriginalFilename());
         } catch (IOException ioe) {
 
         }
-        return null;
+        return "redirect:/e_request/uploadfile";
     }
 }
