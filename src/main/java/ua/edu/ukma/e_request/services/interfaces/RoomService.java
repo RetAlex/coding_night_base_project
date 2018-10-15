9028d147fe6bb0a47263d1322dc4a39f6dab59a4
Oleksii <aar97@ukr.net>
2018-10-15 16:26:07 +0300

    Fixed signature for getAvailableRoomsForEvent

--- src/main/java/ua/edu/ukma/e_request/services/interfaces/RoomService.java
+++ src/main/java/ua/edu/ukma/e_request/services/interfaces/RoomService.java
@@ -1,12 +1,13 @@
 package ua.edu.ukma.e_request.services.interfaces;
 
 
+import java.util.List;
 import java.util.Map;
 
 public interface RoomService {
     /**
      * Returns all rooms that are available for events so student have list to choose from
-     * @return map with string key - name of rooms group owner, and value - description of the room (building + name)
+     * @return map with string key - name of rooms group owner, and value - list of descriptions of room (building + name)
      */
-    Map<String, String> getAvailibleRoomsForEvents();
+    Map<String, List<String>> getAvailibleRoomsForEvents();
 }
